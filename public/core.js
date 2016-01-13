3acaf61a679edbf319c937a58a0ff58fcdafc37f
Nick Wilson <logikfx@gmail.com>
2016-01-13 00:13:00 -0500

    add delete and update functionality works from the front end side

--- public/core.js
+++ public/core.js
@@ -18,6 +18,13 @@ contactApp.factory('dataFactory', ['$http', function($http) {
         return $http.post(urlBase, contact);
     };
     // need others update delete ect.
+    dataFactory.updateContact = function(contact) {
+        console.log("factory contact_id:: " + contact._id);
+        return $http.put(urlBase + '/' + contact._id);
+    };
+    dataFactory.deleteContact = function(id) {
+        return $http.delete(urlBase + '/' + id);
+    };
     return dataFactory;
 }]);
 
@@ -25,13 +32,15 @@ contactApp.controller('mainController', ['$scope', 'dataFactory',
     function($scope, dataFactory) {
     $scope.status;
     $scope.contacts = [];
+
     function getContacts() {
         dataFactory.getContacts().success(function(data) {
             $scope.contacts = data;
             console.log("contact list: " + $scope.contacts);
         })
         .error(function(error) {
-            $scope.status = 'unable to load contacts data: ' + error.message;
+            //$scope.status = 'unable to load contacts data: ' + error.message;
+            console.log('unable to load contact list');
         });
     }
     getContacts(); // so it is called automatically
@@ -44,22 +53,63 @@ contactApp.controller('mainController', ['$scope', 'dataFactory',
             $scope.status = 'unable to retrieve contact';
         });
     } */
-    function addContact() {
-        dataFactory.addContact().success(function(data) {
-            console.log("new contact created");
+
+    $scope.addContact = function addContact() {
+        if (!isEmpty($scope.selected)) { 
+            //console.log("selected id:: " + $scope.selected._id);
+            if ($scope.selected._id == null) {
+                dataFactory.addContact($scope.selected).success(function(data) {
+            
+                    console.log("new contact created " + data.name);
+                })
+                .error(function(error) {
+                    //$scope.status = 'unable to creat new contact: ' + error.message;
+                    console.log('unable to create new contact');
+                });
+                getContacts();
+                formClear();
+            } else {
+                dataFactory.updateContact($scope.selected).success(function(data) {
+                    console.log("contact updated " + data.name);
+                })
+                .error(function(error) {
+                    console.log("contact not updated !!!");
+                    //$scope.status = 'unable to update contact: ' + error.message;
+                });
+                //getContacts();
+                formClear();
+            }
+        }
+    }
+
+    $scope.deleteContact = function deleteContact() {
+        dataFactory.deleteContact($scope.selected._id).success(function(data) {
+            console.log("contact deleted " + $scope.selected.name);
         })
         .error(function(error) {
-            $scope.status = 'unable to creat new contact: ' + error.message;
+            $scope.status = 'unable to delete contact: ' + error.message;
         });
         getContacts();
+        formClear();
     }
+
     $scope.setSelected = function(contact) {
         $scope.selected = this.contact;
         console.log("row name selected: " + $scope.selected.name + $scope.selected.number);
     }
 
     $scope.formClear = function() {
+        formClear();
+    }
+
+    function formClear() {
         $scope.selected = {};
         console.log("Form cleared");
     }
+
+    // test if object is empty
+    function isEmpty(obj) {
+            return Object.keys(obj).length === 0;
+    }
+
 }]);
